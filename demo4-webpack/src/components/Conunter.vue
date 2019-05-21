593e0b33c78b5cf3fd7df33c8aa69b0d24117337
Administrator <11>
2019-05-21 22:20:41 +0800

    init front

---
+++ demo4-webpack/src/components/Conunter.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+      <button @click="increase">+</button>
+      <button @click="decrease">-</button>
+      <span>{{num}}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+      props:["num"],
+      data(){
+        return{
+          num:0
+        }
+      },
+      methods:{
+        increase(){
+         // this.num++;
+          this.$emit('increase')
+        },
+        decrease(){
+          //this.num--;
+          this.$emit('decrease')
+        }
+      }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ demo4-webpack/src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+   <Counter v-bind:num="num" v-on:increase="increase"  v-on:decrease="decrease"></Counter>
+    <span style="color: red">parent:{{num}}</span>
+  </div>
+</template>
+
+<script>
+import Counter from './Conunter';
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      num:20,
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+    Counter
+  },
+  methods:{
+    increase(){
+      this.num++;
+    },
+    decrease(){
+      this.num--;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
